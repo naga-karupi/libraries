5af028ae3cd39449a87be066245beab934ac89b4
Nishinaga Rikuto <toritonsuibu0527@gmail.com>
2022-06-08 08:01:26 +0900

    solved namespace

--- general_purpose_lib/src/PID.cpp
+++ general_purpose_lib/src/PID.cpp
@@ -1,9 +1,9 @@
 #include"PID.hpp"
 
-I_PID::~I_PID(){}
+naga_libs::I_PID::~I_PID(){}
 
 
-float PosType_PID::operator()(){
+float naga_libs::PosType_PID::operator()(){
     const float P = kp*difference.proportional();
     const float I = ki*difference.integral();
     const float D = kd*difference.first_order_difference_equation();
